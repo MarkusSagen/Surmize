b607f88de3e41fbc4b741c4549c9cfec0475c202
nilshugo <nilshugohedberg@gmail.com>
2020-03-31 12:03:57 +0200

    First version model

---
+++ model.py
@@ -0,0 +1,46 @@
+import os 
+import wget
+from cdqa.utils.download import download_squad, download_model, download_bnpp_data
+from cdqa.utils.converters import pdf_converter
+import pandas as pd
+from ast import literal_eval
+from cdqa.pipeline import QAPipeline
+
+# TODO: Change this to the path where articles are stored on the server
+directory = os.getcwd()
+
+"""
+Class for running Q&A on documents
+
+Example use:
+qa = QA()
+answerTuple = qa.question("[-The question-]")
+"""
+class QA:
+    def __init__(self):
+        # Initialize the pool of articles
+        self.df = pdf_converter(directory_path=directory)
+
+        self.cdqa_pipeline = QAPipeline(reader='models/bert_qa.joblib')
+        self.cdqa_pipeline.fit_retriever(df=self.df)
+
+    """
+    Question function
+
+    Inparameter: A text string containing a question
+
+    Returns:
+    A tuple of two strings, first element is the direct answer to the question
+    second element is the sentence/context where the answer was found
+    """
+    def question(self, query):
+        prediction = self.cdqa_pipeline.predict(query)
+
+        answer = prediction[0]
+        context = prediction[2]
+
+        return (answer, context)
+
+qa = QA()
+answerTuple = qa.question("What the hell?")
+print(answerTuple[0])
