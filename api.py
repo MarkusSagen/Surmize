789b0182ad644ab27c7b052900ceae464f122998
sebbersk <seba08_@hotmail.com>
2020-04-03 10:35:10 +0200

    small change

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, flash
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
 
 import os
 from ast import literal_eval
@@ -12,7 +13,17 @@ from model import QA
 app = Flask(__name__)
 CORS(app)
 
-#model = QA()
+# model = QA()
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'doc', 'docx', 'csv'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 dataset_path = os.environ["dataset_path"]
 reader_path = os.environ["reader_path"]
@@ -33,6 +44,26 @@ def test():
         context=context
     )
  """
+
+
+@app.route("/files", methods=["POST"])
+def file():
+
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify(msg='No file part')
+
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        return jsonify(msg='No selected file')
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify(msg="File uploaded")
+    return jsonify(msg="Something went wrong....")
 
 
 @app.route("/", methods=["GET"])
--- client/src/FileForm.js
+++ client/src/FileForm.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
-import Form from './Form'
+
 import Button from './Button'
 
 class FileForm extends Component {
-
+    state = {
+        fileToBeSent: ''
+    }
     handleSubmit = (e) => {
-        console.log("File sent")
+        e.preventDefault();
+        let file = this.state.fileToBeSent;
+        const formData = new FormData();
+        formData.append("file", file);
+        fetch("/uploadfile", {
+            method: 'post',
+            body: formData
+        }).then(resp => resp.json()).then(data => {
+            console.log(data);
+        })
+
+        formData.append("file", file);
+    }
+    fileChange = (e) => {
+        this.setState({ fileToBeSent: e.target.files[0] });
     }
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="exampleInputFile">File input</label>
-                    <input type="file" className="form-control-file" id="exampleInputFile" aria-describedby="fileHelp" />
+                    <input onChange={this.fileChange} type="file" className="form-control-file" id="exampleInputFile" aria-describedby="fileHelp" />
                     <small id="fileHelp" className="form-text text-muted">Supported file types, pdf, doc, docx....</small>
                 </div>
                 <Button type="submit" classN="btn btn-outline-primary" text="Upload File" />
