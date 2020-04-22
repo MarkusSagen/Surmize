b0e5c1aa936004ffaaec6195d9cab43f572dd120
AlexanderBergkvist <alexander.bergkvist@hotmail.com>
2020-04-22 11:21:34 +0200

    j

--- start.sh
+++ start.sh
@@ -12,16 +12,22 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 # Create and activate virtual enviroment
 # sudo pip install -e .
-sudo pip3 install -r requirements.txt --user && echo "${green}Installed dependencies${reset}" || echo "${red}Failed to install dependencies${reset}" 
-python3 -m spacy download en_core_web_sm --user && echo "${green}Downloaded SpaCy english word tokens${reset}" || echo "${red}Failed to download SpaCy english word tokens${reset}" 
+sudo pip3 install -r requirements.txt --user && echo "${green}Installed dependencies${reset}" || echo "${red}Failed to install dependencies${reset}"
+python3 -m spacy download en_core_web_sm --user && echo "${green}Downloaded SpaCy english word tokens${reset}" || echo "${red}Failed to download SpaCy english word tokens${reset}"
+
+#Install textrank (extractive textsummarizer) Wordembeddings
+echo "${green}Downloaded SpaCy english word tokens${reset}" || echo "${red}Failed to download SpaCy english word tokens${reset}"
+cd summarization
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove*.zip
+
+cd ..
 
 # Install the weights
 # Download the datasets, BNP news data
 mkdir data
 cd data
-mkdir uploaded pending examples && echo "${green}Successfully created data folders${reset}" 
+mkdir uploaded pending examples && echo "${green}Successfully created data folders${reset}"
 cd ..
-python3 download_utils.py && echo "${green}Downloaded weights and data${reset}" || echo "${red}Failed to download required weights and data${reset}" 
+python3 download_utils.py && echo "${green}Downloaded weights and data${reset}" || echo "${red}Failed to download required weights and data${reset}"
 echo "Done!"
-
-
